5a12f13f652ab5dbe3e0a53d6320bd697197b991
Igor-Maceno <igorlopesmaceno17@gmail.com>
2025-01-03 13:21:39 -0300

    autentication

--- 11_TOUGHTS/controllers/AuthController.js
+++ 11_TOUGHTS/controllers/AuthController.js
@@ -6,6 +6,34 @@ module.exports = class AuthController {
   static login(req, res) {
     res.render("auth/login");
   }
+  static async loginPost(req, res) {
+    const { email, password } = req.body;
+
+    //find user
+    const user = await User.findOne({ where: { email: email } });
+    if (!user) {
+      req.flash("message", "Usuário não encontrado!");
+      res.render("auth/login");
+
+      return;
+    }
+    //check if password match
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+
+    if (!passwordMatch) {
+      req.flash("message", "Senha inválida!");
+      res.render("auth/login");
+
+      return;
+    }
+    //initializa session
+    req.session.userid = user.id;
+    req.flash("message", "Autenticação realizado com sucesso!");
+
+    req.session.save(() => {
+      res.redirect("/");
+    });
+  }
   static register(req, res) {
     res.render("auth/register");
   }
@@ -49,5 +77,9 @@ module.exports = class AuthController {
     } catch (err) {
       console.log(err);
     }
+  }
+  static logout(req, res) {
+    req.session.destroy();
+    res.redirect("/login");
   }
 };
