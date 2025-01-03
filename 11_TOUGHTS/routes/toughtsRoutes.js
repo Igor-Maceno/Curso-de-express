3ad9b8f8c3efd9f8fe72698da7ae83bd3a0337e3
Igor-Maceno <igorlopesmaceno17@gmail.com>
2025-01-03 13:37:43 -0300

    Auth Middleware

--- 11_TOUGHTS/routes/toughtsRoutes.js
+++ 11_TOUGHTS/routes/toughtsRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const ToughtController = require('../controllers/ToughtsController')
+const ToughtController = require("../controllers/ToughtsController");
 
+// Helpers
+const checkAuth = require("../helpers/auth").checkAuth
+
+router.get("/dashboard", checkAuth, ToughtController.dashboard);
 router.get("/", ToughtController.showToughts);
 
 module.exports = router;
